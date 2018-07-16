e67698eab15324054e3406486364d2d7b3f41458
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-16 12:34:16 +0000

    testar igen

--- dict1.py
+++ dict1.py
@@ -1,4 +1,5 @@
 bra_skivor={'Monster magnet':'Dopes to infinity', 'Sublime':'40oz to freedom', 'Mighty Mighty Bosstones':'Let\'s Face it'}
 
 for y,x in bra_skivor.items():
-	print(x,y)
+    print(y, " gjorde skivan: ", x)
+    
