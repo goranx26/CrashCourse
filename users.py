f0f5dd9e829ab6480f0fc821ce0825d951d01392
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-18 13:53:00 +0000

    added login attempts

--- users.py
+++ users.py
@@ -6,7 +6,7 @@ class User():
         self.last_name = last_name
         self.login_name = login_name
         self.title = title
-        
+        self.login_attempts = 0        
 
     def describe_user(self):
         print("Name: " + self.first_name.title()+ " " +self.last_name.title())
@@ -17,9 +17,24 @@ class User():
         print("Welcome " + self.title.title() + " " +self.first_name.title())
         print("Have a pleasant stay today.")
 
+    def increment_login_attempts(self):
+         self.login_attempts += 1 
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 goran = User('goran','andersson','root','unethical hacker')
 
 goran.greet_user()
 
 goran.describe_user()
+
+i=1
+while i<10:
+    goran.increment_login_attempts()
+    print("Login attempts: " + str(goran.login_attempts))
+    i += 1
+
+goran.reset_login_attempts()
+print("RESET, login attempts now: " + str(goran.login_attempts))
