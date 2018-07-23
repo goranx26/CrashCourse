e7f2a7c5817c8a9eb5c9dab715e6f80be135884c
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-23 06:56:21 +0200

    keeping on keeping on

--- guestbook.py
+++ guestbook.py
@@ -2,10 +2,9 @@
 filename = 'guestbook.txt'
 
 while True:
-    name = input("What is your name?: ")
-    message = input("Type a short message for the other guest \(type 'q' to quit\): ")
-    if name != 'q' or message != 'q':
-        entry = (name + "\n" + message + "\n\n")
+    message = input("What do you like about programming (type 'q' to quit): ")
+    if  message != 'q':
+        entry = (message + "\n")
         with open(filename, 'a') as file_object:
             file_object.write(entry)
     else:
