b987e1bdc8a0f6545ff1b3d8d0528ad6d3c9a905
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-24 12:25:04 +0000

    Look ma, I made a dice!

---
+++ division.py
@@ -0,0 +1,17 @@
+print("Type two numbers to be divided")
+print("Type 'q' to quit")
+
+while True:
+    num1 = input("first number:")
+    if num1 == 'q':
+        break
+    num2 = input("second number")
+    if num2 == 'q':
+        break
+    try:
+        answer = (int(num1) / int(num2))
+    except ZeroDivisionError:
+        print("You can not divide by 0")
+    else:
+        print(answer)
+
