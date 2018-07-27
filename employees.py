e09cdcb6fbc7a1ed335942647993f8e155e2ac2c
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-27 22:12:50 +0200

    struggling on

--- employees.py
+++ employees.py
@@ -3,14 +3,13 @@ class Employee():
 
     def __init__(self, fname, lname, salary):
         """Initialising with the given attributes"""
-        fname = self.fname
-        lname = self.lname
-        salary = self.salary
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
 
-    
     def give_raise(self, payraise='5000'):
         """Raises the salary (standard raise = $5000)"""
-        salary += payraise
+        self.salary += payraise
 
 
 
--- test_employees.py
+++ test_employees.py
@@ -1,21 +1,20 @@
 import unittest
 from employees import Employee
 
+
 class TestEmployees(unittest.TestCase):
     """Tests for employees.py"""
 
     def SetUp(self):
-        test_dude = Employee('Test', 'Dude', 5000)
-
+        self.test_dude = Employee('Test', 'Dude', 5000)
 
     def test_give_default_raise(self):
-        test_dude.give_raise()
-        self.assertEqual(test_dude.salary, 10000)
-
+        self.test_dude.give_raise()
+        self.assertEqual(self.test_dude.salary, 10000)
 
     def test_give_custom_raise(self):
-        test_dude.give_raise(10000)
-        self.assertEqual(test_dude.salary, 15000)
+        self.test_dude.give_raise(10000)
+        self.assertEqual(self.test_dude.salary, 15000)
 
 
 unittest.main()
