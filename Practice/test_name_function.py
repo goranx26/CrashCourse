6c8ec8db36f3112883f6a11f0e8b2998b788734d
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-27 10:23:51 +0000

    Adding tests

--- Practice/test_name_function.py
+++ Practice/test_name_function.py
@@ -5,9 +5,14 @@ from name_function import get_formatted_name
 class NamesTestCase(unittest.TestCase):
     """Tests for name_function.py"""
     def test_first_last_name(self):
-        """Do names like Janis joplin work?"""
-        formatted_name = get_formatted_name('janis', 'joplin')
-        self.assertEqual(formatted_name, 'Janis Joplin')
+        """Do names like 'Jimmy Gestapo' work?"""
+        formatted_name = get_formatted_name('jimmy', 'gestapo')
+        self.assertEqual(formatted_name, 'Jimmy Gestapo')
+
+    def test_first_last_middle_name(self):
+        """Do name like 'Maynard James Keenan" work?"""
+        formatted_name = get_formatted_name('maynard', 'keenan', 'james')
+        self.assertEqual(formatted_name, 'Maynard James Keenan')
+
 
 unittest.main()
-    
