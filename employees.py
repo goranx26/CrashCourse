a11b64c20bf917b766fbc29885a8556319d342e2
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-07-27 20:03:32 +0000

    fuck this

--- employees.py
+++ employees.py
@@ -1,15 +1,16 @@
-class Employee(self, fname, lname, salary):
+class Employee():
     """Class to create employees"""
 
-    def __init__(self):
+    def __init__(self, fname, lname, salary):
         """Initialising with the given attributes"""
         fname = self.fname
         lname = self.lname
         salary = self.salary
 
-    def give_raise(self, raise=5000):
+    
+    def give_raise(self, payraise='5000'):
         """Raises the salary (standard raise = $5000)"""
-        salary += raise
+        salary += payraise
 
 
 
--- test_employees.py
+++ test_employees.py
@@ -1,19 +1,22 @@
 import unittest
-from employees import give_raise
+from employees import Employee
 
 class TestEmployees(unittest.TestCase):
     """Tests for employees.py"""
 
     def SetUp(self):
-        self.test_dude = Employee('Test', 'Dude', 5000)
+        test_dude = Employee('Test', 'Dude', 5000)
 
 
     def test_give_default_raise(self):
-        self.test_dude.give_raise()
-        self.assertEqual(self.testdude.salary, 10000)
+        test_dude.give_raise()
+        self.assertEqual(test_dude.salary, 10000)
+
 
     def test_give_custom_raise(self):
-        pass
+        test_dude.give_raise(10000)
+        self.assertEqual(test_dude.salary, 15000)
 
 
+unittest.main()
 
